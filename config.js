162d85bf9d7c5683ce4a85eb964878f1a87334b4
callmepazuzu <102613427+callmepazuzu@users.noreply.github.com>
2022-03-30 00:28:52 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const  CONFIG  =  {
+    titleWeb : "SOS" ,
+    introTitle : 'HELLO' ,
+    introDesc : Pazuzu xin chào quý khách` ,
+    btnIntro : 'Ấn tiếp đi nè' ,
+    title : 'Em ăn cơm chưa? ngày hôm nay của em thế nào =))' ,
+    desc : 'Em muốn chúng ta đi đâu chơi nè ??' ,
+    btn Có : 'Đi ks lun' ,
+    btnNo : 'hong bé ơi' ,
+    question : 'Vậy là đi nhà nghỉ thiệt luôn hả :)))' ,
+    btnReply : 'IB liền cho Anh' ,
+    reply : 'Dạ, mình đi ks ạ :)))' ,
+    mess : 'Anh biết mà :)) ok iêm anh qua liền á ' ,
+    messDesc : 'Nhấn vào ok để ib liền cho anh nè " ,
+    btn Chấp nhận : 'Oki' ,
+    messLink : 'https://github.com/zukahai/Confess-Crush'//https://www.facebook.com/messages/t/100075447270628
+}
